db2b746c4472bae0b8b78bb82bb795be9c22a2cc
Seang-G <mgw7730@gmail.com>
2021-11-19 21:12:05 +0900

    `21.11.19.
    HTML 기본 틀 제작 (미완)
    HTML과 Flask 간의 데이터 전송 구현

--- data.py
+++ data.py
@@ -1,39 +1,50 @@
 from extractor import saramin, indeed, jobkorea
 import pandas as pd
+from datetime import datetime
 import os
 import time
 
-start_time = time.time()
-
-os.chdir("파이썬 응용/텀 프로젝트/Search_job")
 sites = ['Saramin', 'Indeed', 'JobKorea']
 languages = ['C', 'C++', 'C#', 'Java', 'JavaScript', 'Python', 'Go']
 
-# 엑셀 파일 생성
-writer = pd.ExcelWriter('data.xlsx', engine='xlsxwriter')
+def save():
+    global sites
+    global languages
 
-total_lst = []
-for language in languages:
-    # 검색된 채용 정보의 수 추출
-    total_lst.append([saramin.get_total(language),
-                    indeed.get_total(language),
-                    jobkorea.get_total(language)])
+    start_time = time.time()
+    home = os.path.dirname(os.path.realpath(__file__))
 
-    # 채용 정보 추출
-    df_saramin = pd.DataFrame(saramin.extract_jobs(language), columns=['Title', 'Location', 'Company', 'URL'])
-    df_indeed = pd.DataFrame(indeed.extract_jobs(language), columns=['Title', 'Location', 'Company', 'URL'])
-    df_jobkorea = pd.DataFrame(jobkorea.extract_jobs(language), columns=['Title', 'Location', 'Company', 'URL'])
+    # 엑셀 파일 생성
+    now = datetime.now().strftime('%Y%m%d')
+    writer = pd.ExcelWriter(f'home\\data\\{now}.xlsx', engine='xlsxwriter')
 
-    # 채용 정보를 엑셀로 저장
-    df_saramin.to_excel(writer, sheet_name=f'Saramin_{language}')
-    df_indeed.to_excel(writer, sheet_name=f'Indeed_{language}')
-    df_jobkorea.to_excel(writer, sheet_name=f'JobKorea_{language}')
+    total_lst = []
+    for language in languages:
+        # 검색된 채용 정보의 수 추출
+        total_lst.append([saramin.get_total(language),
+                        indeed.get_total(language),
+                        jobkorea.get_total(language)])
 
-# 채용 정보의 수를 엑셀로 저장
-df_total = pd.DataFrame(total_lst, index=languages, columns=sites)
-df_total.to_excel(writer, sheet_name='Total')
+        # 채용 정보 추출
+        df_saramin = pd.DataFrame(saramin.extract_jobs(language), columns=['Title', 'Location', 'Company', 'URL'])
+        df_indeed = pd.DataFrame(indeed.extract_jobs(language), columns=['Title', 'Location', 'Company', 'URL'])
+        df_jobkorea = pd.DataFrame(jobkorea.extract_jobs(language), columns=['Title', 'Location', 'Company', 'URL'])
 
-writer.save()
+        # 채용 정보를 엑셀로 저장
+        df_saramin.to_excel(writer, sheet_name=f'Saramin_{language}', index=False)
+        df_indeed.to_excel(writer, sheet_name=f'Indeed_{language}', index=False)
+        df_jobkorea.to_excel(writer, sheet_name=f'JobKorea_{language}', index=False)
 
-total_time = int(time.time() - start_time)
-print(f'걸린 시간 : {total_time//60}분 {total_time%60}초')
+    # 채용 정보의 수를 엑셀로 저장
+    df_total = pd.DataFrame(total_lst, index=languages, columns=sites)
+    df_total.to_excel(writer, sheet_name='Total')
+
+    writer.save()
+
+    total_time = int(time.time() - start_time)
+    print(f'걸린 시간 : {total_time//60}분 {total_time%60}초')
+
+def get_bdata():
+    global sites
+    global languages
+    return languages, sites
--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
+from itertools import product
+import pandas as pd
+import data
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', languages=languages, sites=sites)
+
+@app.route('/result')
+def result():
+    selected_sites = [site for site in sites if request.args.get(site) is not None]
+    selected_languages = [language for language in languages if request.args.get(language) is not None]
+
+    sites_n_languages = list(product(selected_sites, selected_languages))
+    df_dic = {}
+    print(sites_n_languages)
+    for site_n_language in sites_n_languages:
+        site, language = site_n_language
+        df = pd.read_excel(f'{home}\\data\\{now}.xlsx', f'{site}_{language}')
+        df_dic[site_n_language] = df
+
+    # return render_template('result.html', tables=df_lst)
+    return df.to_html()
 
 if __name__ == '__main__':
-    app.run()
+    now = datetime.now().strftime('%Y%m%d')
+    home = os.path.dirname(os.path.realpath(__file__))
+    if f'{now}.xlsx' in os.listdir(home+'\\data'):
+        languages, sites = data.get_bdata()
+        app.run()
+
+    else: data.save()
