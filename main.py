677b4a47faedbf3d7b7326d270ee11758d707a3c
Seang-G <mgw7730@gmail.com>
2021-12-12 17:29:55 +0900

    `21.12.12.
    간결화 작업

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from datetime import datetime
-from itertools import product
 import pandas as pd
 import data
 import os
@@ -13,27 +12,13 @@ def hello_world():
 
 @app.route('/result')
 def result():
-    # selected_sites = [site for site in sites if request.args.get(site) is not None]
-    # selected_languages = [language for language in languages if request.args.get(language) is not None]
-
-    # sites_n_languages = list(product(selected_sites, selected_languages))
-    # df_dic = {}
-    # for site_n_language in sites_n_languages:
-    #     site, language = site_n_language
-    #     df = pd.read_excel(f'{home}\\data\\{now}.xlsx', f'{site}_{language}')
-    #     df_dic[site_n_language] = df
     selected_site = request.args.get('site')
     selected_language = request.args.get('language')
     df = pd.read_excel(f'{home}\\data\\{now}.xlsx', f'{selected_site}_{selected_language}')
 
-    return render_template('result.html', site=selected_site, language=selected_language)
+    return make_table(df[:50].to_html(), selected_language, selected_site)
 
-@app.route('/Saramin_C')
-def show():
-    df = pd.read_excel(f'{home}\\data\\{now}.xlsx', 'Saramin_C')
-    return make_table_good(df[:10].to_html())
-
-def make_table_good(table):
+def make_table(table, language, site):
     return f'''
             <!DOCTYPE html>
             <html>
@@ -41,7 +26,15 @@ def make_table_good(table):
                 <link href="https://andybrewer.github.io/mvp/mvp.css" rel="stylesheet"></link>
             </head>
             <body>
-                {table}
+                    <header>
+                        <a href="/">
+                            <h3>Back to main</h3>
+                        </a>
+                    </header>
+                <h2 align='center'>{language} in {site}</h2>
+                <div align='center'>
+                    {table}
+                </div>
             </body>
             </html>
             '''
